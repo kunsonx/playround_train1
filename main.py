dbb97e3dc0eaacaa71d0c9217b146a82335cb08f
jon <jon@jons-Mac-Pro.local>
2021-05-12 04:08:51 +0800

    feat : update

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+# Import mlcompute module to use the optional set_mlc_device API for device selection with ML
+# Compute.
+from tensorflow.python.compiler.mlcompute import mlcompute
+
+# Select Any device.
+mlcompute.set_mlc_device(device_name='any')  # Available options are 'cpu', 'gpu', and 'any'.
+
+# mlcompute.set_mlc_device(device_name='gpu')
+print("is_apple_mlc_enabled %s" % mlcompute.is_apple_mlc_enabled())
+print("is_tf_compiled_with_apple_mlc %s" % mlcompute.is_tf_compiled_with_apple_mlc())
+
 import pandas as pd
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import collections
+
 from tensorflow import keras
 from tensorflow.keras import layers
+
+tf.compat.v1.disable_eager_execution()
+
+print(f"eagerly? {tf.executing_eagerly()}")
+print(tf.config.list_logical_devices())
+
 from datetime import datetime
 
 print(tf.__version__)
-
-tf.config.threading.set_intra_op_parallelism_threads(16)
-tf.config.threading.set_inter_op_parallelism_threads(16)
 
 
 # 加载数据
@@ -178,3 +193,8 @@ def get_model(num_users, num_genders, num_ages, num_occupations, num_movies, num
 
     return keras.models.Model(inputs=[user_id, gender, age, occupation, movie_id, genre],
                               outputs=[output])
+
+
+if __name__ == "__main__":
+    d = load_that_data()
+    train(d)
